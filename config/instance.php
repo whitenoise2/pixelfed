c042d13590c0f6fd77f2f7f47e2f68ae4412a5b9
Daniel Supernault <danielsupernault@gmail.com>
2024-07-21 01:52:14 -0600

    Update instance config, update network cache feed max_hours_old falloff to 90 days instead of 6 hours to allow for less active instances to have more results

--- config/instance.php
+++ config/instance.php
@@ -36,7 +36,7 @@ return [
         'network' => [
             'cached' => env('PF_NETWORK_TIMELINE') ? env('INSTANCE_NETWORK_TIMELINE_CACHED', false) : false,
             'cache_dropoff' => env('INSTANCE_NETWORK_TIMELINE_CACHE_DROPOFF', 100),
-            'max_hours_old' => env('INSTANCE_NETWORK_TIMELINE_CACHE_MAX_HOUR_INGEST', 6),
+            'max_hours_old' => env('INSTANCE_NETWORK_TIMELINE_CACHE_MAX_HOUR_INGEST', 2160),
         ],
     ],
 
